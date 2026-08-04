ac743dfd84ca7c3036c9328a8ee8f8217b062308
minghinshi <mhshi9768@gmail.com>
2026-08-04 11:20:11 +0800

    Set NODE_ENV=test when testing

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
@@ -5,4 +7,11 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  // Make sure NODE_ENV is not set to production
+  // Some test helpers, like act(), are unavailable in production
+  test: {
+    env: {
+      NODE_ENV: "test",
+    },
+  },
 });
